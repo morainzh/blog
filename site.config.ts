3e67c97795acf287f206b91d6a8f41e4bd9b5dbd
morainzh <32979611+morainzh@users.noreply.github.com>
2023-08-09 17:01:01 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,16 +2,16 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '73e3d65c8ea0441c87b9c861e56a4a60',
+  rootNotionPageId: '2a51162e51314b1f832a4c97d8b24d9e',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
+  name: 'morainzh的博客',
   domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  author: 'zhao',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
